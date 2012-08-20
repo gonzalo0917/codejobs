1542e9c3e20155cd2204de632a2e8e3983ffb8a2
Carlos Santana <carlos@milkzoft.com>
2012-08-19 23:40:17 -0500

    Ad

--- www/applications/blog/views/post.php
+++ www/applications/blog/views/post.php
@@ -35,6 +35,18 @@
 				<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-5026e83358e73317"></script>
 
 				<?php echo showContent(bbCode($post["Content"], $URL)); ?>
+				<br />
+				<script type="text/javascript"><!--
+					google_ad_client = "ca-pub-4006994369722584";
+					/* CodeJobs.biz */
+					google_ad_slot = "1672839256";
+					google_ad_width = 728;
+					google_ad_height = 90;
+					//-->
+					</script>
+					<script type="text/javascript"
+					src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+					</script>
 			</div>
 
 			<div class="post-social">		
