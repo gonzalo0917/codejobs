bf626633ef4c15b4a6105b84878610ccad7150ec
Carlos Santana <carlos@milkzoft.com>
2012-08-19 23:45:43 -0500

    Statcounter code

--- www/lib/themes/newcodejobs/footer.php
+++ www/lib/themes/newcodejobs/footer.php
@@ -15,5 +15,13 @@
         </footer>
 
         <?php print $this->getJs(); ?>
+
+        <script type="text/javascript">
+        var sc_project=7655788; 
+        var sc_invisible=1; 
+        var sc_security="f167f55b"; 
+        </script>
+        <script type="text/javascript"
+        src="http://www.statcounter.com/counter/counter.js"></script>
     </body>
 </html>
