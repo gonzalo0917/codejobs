973cb6a45fd4de8e480360821e2ca13da2643537
Carlos Santana <carlos@milkzoft.com>
2012-08-26 01:55:08 -0500

    Advertising and contact us links on footer actived

--- www/lib/themes/newcodejobs/footer.php
+++ www/lib/themes/newcodejobs/footer.php
@@ -2,15 +2,20 @@
 
         <footer>
             <p>
-                <a href="<?php print path(slug(__("Advertising")), FALSE, FALSE); ?>"><?php print __("Advertising"); ?></a> &nbsp;&nbsp;
-                <!--<a href="<?php print path(slug(__(_("Legal notice"))), FALSE, FALSE); ?>"><?php print __(_("Legal notice")); ?></a>  &nbsp;&nbsp;-->
-                <!--<a href="<?php print path(slug(__(_("Terms of Use"))), FALSE, FALSE); ?>"><?php print __(_("Terms of Use")); ?></a>  &nbsp;&nbsp;-->
-                <!--<a href="<?php print path(slug(__(_("About CodeJobs"))), FALSE, FALSE); ?>"><?php print __(_("About CodeJobs")); ?></a> &nbsp;&nbsp;-->
-        		<a href="<?php print path("feedback"); ?>"><?php print __("Contact us"); ?></a><br />
-
-               <?php print __(_("This site is licensed under a")); ?> 
-                <a href="http://creativecommons.org/licenses/by/3.0/" target="_blank">Creative Commons Attribution 3.0 License</a>. 
-                <?php print __(_("Powered by")); ?> <a href="http://www.milkzoft.com" target="_blank">MilkZoft</a>
+                <?php 
+                    if(whichLanguage() === "Spanish") {
+                    ?>
+                        <a href="<?php print path(slug(__("Advertising")), FALSE, FALSE); ?>"><?php print __("Advertising"); ?></a> &nbsp;&nbsp;
+                        <!--<a href="<?php print path(slug(__(_("Legal notice"))), FALSE, FALSE); ?>"><?php print __(_("Legal notice")); ?></a>  &nbsp;&nbsp;-->
+                        <!--<a href="<?php print path(slug(__(_("Terms of Use"))), FALSE, FALSE); ?>"><?php print __(_("Terms of Use")); ?></a>  &nbsp;&nbsp;-->
+                        <!--<a href="<?php print path(slug(__(_("About CodeJobs"))), FALSE, FALSE); ?>"><?php print __(_("About CodeJobs")); ?></a> &nbsp;&nbsp;-->
+                		<a href="<?php print path("feedback"); ?>"><?php print __("Contact us"); ?></a><br />
+                    <?php
+                    }
+                    
+                    print __(_("This site is licensed under a")); ?> 
+                    <a href="http://creativecommons.org/licenses/by/3.0/" target="_blank">Creative Commons Attribution 3.0 License</a>. 
+                    <?php print __(_("Powered by")); ?> <a href="http://www.milkzoft.com" target="_blank">MilkZoft</a>
             </p>
         </footer>
 
