9299a99bec24230eb56e996bb7d71d0a425e7524
Carlos Santana <carlos@milkzoft.com>
2012-08-26 01:30:41 -0500

    Feedback application fixed

--- www/lib/languages/spanish.php
+++ www/lib/languages/spanish.php
@@ -209,7 +209,7 @@ $phrase["october"]   = "Octubre";
 $phrase["november"]  = "Noviembre";
 $phrase["december"]  = "Diciembre";
 
-$phrase["contact_us_today"] = "Póngase en contacto con nosotros hoy";
+$phrase["contact_us_today"] = "¡Póngase en contacto con nosotros hoy mismo!";
 $phrase["your_message_has_been_sent_successfully_we_will_contact_you_as_soon_as_possible_thank_you_very_much"] = "Tu mensaje ha sido enviado con éxito, nos pondremos en contacto contigo lo antes posible, ¡muchas gracias!";
 $phrase["message"] = "Mensaje";
 $phrase["send_my_message"] = "Enviar mi mensaje";
