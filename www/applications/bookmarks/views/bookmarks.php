71027da0acd3d8cd2eebc14ba582d4c3389baa34
Carlos Santana <carlos@milkzoft.com>
2012-09-05 17:56:53 -0500

    Ads randomized on Bookmarks

--- www/applications/bookmarks/views/bookmarks.php
+++ www/applications/bookmarks/views/bookmarks.php
@@ -53,26 +53,27 @@
 			<?php 
 				} 
 			?>
-			<br />
-				<?php					
-					if(get("production") and ($i === $rand1 or $i === $rand2)) {
-						?>
-							<p>
-								<script type="text/javascript"><!--
-									google_ad_client = "ca-pub-4006994369722584";
-									/* CodeJobs.biz */
-									google_ad_slot = "1672839256";
-									google_ad_width = 728;
-									google_ad_height = 90;
-									//-->
-									</script>
-									<script type="text/javascript"
-									src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+			
+			<?php					
+				if(get("production") and ($i === $rand1 or $i === $rand2)) {
+					?>
+						<p>
+							<script type="text/javascript"><!--
+								google_ad_client = "ca-pub-4006994369722584";
+								/* CodeJobs.biz */
+								google_ad_slot = "1672839256";
+								google_ad_width = 728;
+								google_ad_height = 90;
+								//-->
 								</script>
-							</p>
-						<?php
-					}
-				?>			
+								<script type="text/javascript"
+								src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+							</script>
+						</p>
+					<?php
+				}
+			?>			
+			<br />
 	<?php 
 		} 
 	?>
