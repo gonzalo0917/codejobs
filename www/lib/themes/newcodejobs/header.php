4fef8a7bab03f80fd5b73f821c51754e2c0f25f4
Carlos Santana <carlos@milkzoft.com>
2012-09-05 17:15:56 -0500

    Codes activated on Navbar

--- www/lib/themes/newcodejobs/header.php
+++ www/lib/themes/newcodejobs/header.php
@@ -68,7 +68,7 @@
 				<nav>
 					<ul>
 						<li><a href="<?php echo path(); ?>"><?php echo __("Home"); ?></a></li>
-						<!--<li><a href="<?php echo path("codes"); ?>"><?php echo __("Codes"); ?></a></li>-->
+						<li><a href="<?php echo path("codes"); ?>"><?php echo __("Codes"); ?></a></li>
 						<!--<li><a href="<?php echo path("jobs"); ?>"><?php echo __("Jobs"); ?></a></li>-->
 						<!--<li><a href="<?php echo path("forums"); ?>"><?php echo __("Forums"); ?></a></li>-->
 						<li><a href="http://www.youtube.com/codejobs" target="_blank"><?php echo __("Videos"); ?></a></li>
