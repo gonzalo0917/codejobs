10cef4ac720729845313f8216dc28014e79ef0a3
Carlos Santana <carlos@milkzoft.com>
2012-09-12 16:14:59 -0500

    Links moved to footer

--- www/lib/themes/newcodejobs/footer.php
+++ www/lib/themes/newcodejobs/footer.php
@@ -5,21 +5,22 @@
                 <?php 
                     if(whichLanguage() === "Spanish") {
                     ?>
-                        <a href="<?php print path(slug(__("Advertising")), FALSE, FALSE); ?>"><?php print __("Advertising"); ?></a> &nbsp;&nbsp;
-                        <!--<a href="<?php print path(slug(__(_("Legal notice"))), FALSE, FALSE); ?>"><?php print __(_("Legal notice")); ?></a>  &nbsp;&nbsp;-->
-                        <!--<a href="<?php print path(slug(__(_("Terms of Use"))), FALSE, FALSE); ?>"><?php print __(_("Terms of Use")); ?></a>  &nbsp;&nbsp;-->
-                        <!--<a href="<?php print path(slug(__(_("About CodeJobs"))), FALSE, FALSE); ?>"><?php print __(_("About CodeJobs")); ?></a> &nbsp;&nbsp;-->
-                		<a href="<?php print path("feedback"); ?>"><?php print __("Contact us"); ?></a><br />
+                        <a href="<?php echo path(slug(__("Advertising")), FALSE, FALSE); ?>"><?php echo __("Advertising"); ?></a> &nbsp;&nbsp;
+                        <!--<a href="<?php echo path(slug(__(_("Legal notice"))), FALSE, FALSE); ?>"><?php echo __(_("Legal notice")); ?></a>  &nbsp;&nbsp;-->
+                        <!--<a href="<?php echo path(slug(__(_("Terms of Use"))), FALSE, FALSE); ?>"><?php echo __(_("Terms of Use")); ?></a>  &nbsp;&nbsp;-->
+                        <!--<a href="<?php echo path(slug(__(_("About CodeJobs"))), FALSE, FALSE); ?>"><?php echo __(_("About CodeJobs")); ?></a> &nbsp;&nbsp;-->
+                        <a href="<?php echo path("links", TRUE); ?>"><?php echo __("Links"); ?></a>
+                		<a href="<?php echo path("feedback"); ?>"><?php echo __("Contact us"); ?></a><br />
                     <?php
                     }
                     
-                    print __(_("This site is licensed under a")); ?> 
+                    echo __(_("This site is licensed under a")); ?> 
                     <a href="http://creativecommons.org/licenses/by/3.0/" target="_blank">Creative Commons Attribution 3.0 License</a>. 
-                    <?php print __(_("Powered by")); ?> <a href="http://www.milkzoft.com" target="_blank">MilkZoft</a>
+                    <?php echo __(_("Powered by")); ?> <a href="http://www.milkzoft.com" target="_blank">MilkZoft</a>
             </p>
         </footer>
 
-        <?php print $this->getJs(); ?>
+        <?php echo $this->getJs(); ?>
 
         <?php
             if(get("production")) {
