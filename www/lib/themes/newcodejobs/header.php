a500e9d497a0620a6aa45b96ae113d42c10a4ba1
Carlos Santana <carlos@milkzoft.com>
2012-09-13 15:38:39 -0500

    Mediaqueries updated

--- www/lib/themes/newcodejobs/header.php
+++ www/lib/themes/newcodejobs/header.php
@@ -5,7 +5,7 @@
 <html lang="<?php echo get("webLang"); ?>"<?php echo defined("_angularjs") ? " ng-app" : "";?>>
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<!--<meta name="viewport" content="width=device-width, initial-scale=1.0" />-->
 	<?php echo $this->getMeta(); ?>
 	<title><?php echo $this->getTitle(); ?></title>
 	<link href="<?php echo path("blog/rss"); ?>" rel="alternate" type="application/rss+xml" title="RSS <?php echo __("Blog"); ?>" />
