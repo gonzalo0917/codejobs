d98208e32c3cd8d98596d4fe839bce48844a9329
Carlos Santana <carlos@milkzoft.com>
2012-09-19 19:05:01 -0500

    Popular posts

--- www/applications/blog/views/popular.php
+++ www/applications/blog/views/popular.php
@@ -5,8 +5,10 @@
 	<?php
 		foreach($posts as $post) {
 			$URL = path("blog/". $post["Year"] ."/". $post["Month"] ."/". $post["Day"] ."/". $post["Slug"]);
+
+			$views = ceil($post["Views"] / 2);
 		?>
-			<li><a href="<?php echo $URL; ?>" title="<?php echo __("Readings") .": ". $post["Views"]; ?>"><?php echo $post["Title"]; ?></a></li></li>
+			<li><a href="<?php echo $URL; ?>" title="<?php echo __("Readings") .": ". $views; ?>"><?php echo $post["Title"]; ?></a></li></li>
 		<?php
 		}
 	?>
