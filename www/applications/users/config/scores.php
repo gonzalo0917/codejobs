728794e8bc45d716516a16be4b2f172adef64767
Jhon <yonclev@gmail.com>
2012-10-02 16:23:54 -0500

    #55 Config file added

---
+++ www/applications/users/config/scores.php
@@ -0,0 +1,12 @@
+<?php
+
+if(!defined("_access")) {
+	die("Error: You don't have permission to access here...");
+}
+
+define("_blogCredits", 3);
+define("_blogRecommendations", 5);
+define("_codesCredits", 2);
+define("_codesRecommendations", 3);
+define("_bookmarksCredits", 1);
+define("_bookmarksRecommendations", 1);
