27471e63da7ba3f72a79bc5e02f619226a314dc6
Carlos Santana <carlos@milkzoft.com>
2012-10-06 10:57:23 -0500

    CodeJobsTV

--- www/applications/pages/views/tv.php
+++ www/applications/pages/views/tv.php
@@ -35,8 +35,8 @@
 		</header>
 		
 		<div id="content">
-			<iframe width="924" height="520" src="http://www.youtube.com/embed/uOToJW4x2GE" frameborder="0" allowfullscreen></iframe>
-
+			<iframe width="924" height="520" src="http://www.youtube.com/embed/inRwevJEs0A" frameborder="0" allowfullscreen></iframe>
+			
 			<div class="ads">
 				<script type="text/javascript"><!--
 				google_ad_client = "ca-pub-4006994369722584";
