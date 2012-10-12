133c73b5ffa78db5cc1f8df411052468b94f932f
Carlos Santana <carlos@milkzoft.com>
2012-10-12 16:59:57 -0500

    ModRewrite fixed

--- zan/helpers/router.php
+++ zan/helpers/router.php
@@ -347,7 +347,6 @@ function whichApplication() {
 }
 
 function path($path = FALSE, $URL = FALSE, $lang = TRUE) {
-	die(_get("webBase"));
 	if(!$path) {
 		return isLang() ? _get("webBase") ."/". _get("webLang") : _get("webBase");
 	} 
