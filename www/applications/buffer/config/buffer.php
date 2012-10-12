79495bbe62109ef0083d8e8874097be64f777a9e
Carlos Santana <carlos@milkzoft.com>
2012-10-12 12:58:31 -0500

    Buffer App added

---
+++ www/applications/buffer/config/buffer.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Access from index.php:
+ */
+if(!defined("_access")) {
+	die("Error: You don't have permission to access here...");
+}
+
+define("_bufferToken", "1/ccdb3b0510bde73400b9dcb2695a2812");
