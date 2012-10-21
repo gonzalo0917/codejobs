c877ca25dfc7561138024dc175d4333d61d0b0bb
Jhon <yonclev@gmail.com>
2012-10-20 20:17:46 -0500

    #50 (1) Códigos por usuario

--- www/applications/codes/views/codes.php
+++ www/applications/codes/views/codes.php
@@ -40,7 +40,7 @@
 			</span>
 
 			<div class="addthis_toolbox addthis_default_style ">
-				<a class="addthis_button_tweet" tw:via="codejobs" addthis:title="#Code <?php echo $code["Title"]; ?>" tw:url="<?php echo $URL; ?>"></a>
+				<a class="addthis_button_tweet" tw:via="codejobs" addthis:title="#Code <?php echo $code["Title"]; ?>" tw:url="<?php echo path("codes/". $code["ID_Code"] ."/". $code["Slug"], FALSE, $code["Language"]); ?>"></a>
 			</div>
 
 			<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
