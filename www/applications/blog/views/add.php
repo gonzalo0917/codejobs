599b71142f05acc54ac5234bd3dad5461f44aa45
Carlos Santana <carlos@milkzoft.com>
2012-10-22 12:27:45 -0500

    Autosaver fixed

--- www/applications/blog/views/add.php
+++ www/applications/blog/views/add.php
@@ -119,7 +119,7 @@
 			echo formInput(array("id" => "code", 		 "name" => "code", 			"type" => "hidden", "value" => code(10)));
 			echo formInput(array("id" => "temp_title", 	 "name" => "temp_title", 	"type" => "hidden", "value" => addslashes($title)));
 			echo formInput(array("id" => "temp_tags", 	 "name" => "temp_tags", 	"type" => "hidden", "value" => $tags));
-			echo formInput(array("id" => "temp_content", "name" => "temp_content",  "type" => "hidden", "value" => addslashes($content)));
+			echo formInput(array("id" => "temp_content", "name" => "temp_content",  "type" => "hidden", "value" => addslashes("Hola")));
 
 		echo formClose();
 	echo div(FALSE);
