6bb691b6518d373d37a8e188d288d549542f8427
Jhon <yonclev@gmail.com>
2012-10-22 20:18:54 -0500

    #47 (1) Editar perfil [IN PROGRESS]

--- www/applications/users/views/profile.php
+++ www/applications/users/views/profile.php
@@ -2,9 +2,9 @@
 
 <div class="editProfile">
 	<?php if(_get("webLang") === "en") { ?>
-		<p class="center"><?php echo $user[0][0]["Username"] . "'s Profile";?></p>
+		<p class="center"><?php echo $user["Username"] . "'s Profile";?></p>
 	<?php } elseif(_get("webLang") === "es") { ?>
-		<p class="center"><?php echo "Perfil de " . $user[0][0]["Username"];?></p>
+		<p class="center"><?php echo "Perfil de " . $user["Username"];?></p>
 	<?php } ?>
 	
 	<?php echo isset($alert) ? $alert : NULL; ?>
@@ -13,26 +13,26 @@
 		<p class="title main"><?php echo __("Profile");?></p>
 		
 		<div class="avatar">
-			<div id="avatar"><img src="<?php echo $avatar;?>" title="<?php echo $user[0][0]["Username"];?>" alt="<?php echo $user[0][0]["Username"];?>" /></div><br />
+			<div id="avatar"><img src="<?php echo $avatar;?>" title="<?php echo $user["Username"];?>" alt="<?php echo $user["Username"];?>" /></div><br />
 		</div>
 		
 		<div class="social">
 			<?php if($twitter) { ?>
-				<a class="sn" id="twitter" rel="external" href="http://twitter.com/<?php echo $user[1][0]["Twitter"];?>" title="<?php echo $user[1][0]["Twitter"];?>">	<img src="<?php echo $twitter;?>" alt="twitter.com"/>
+				<a class="sn" id="twitter" rel="external" href="http://twitter.com/<?php echo $user["Twitter"];?>" title="<?php echo $user["Twitter"];?>">	<img src="<?php echo $twitter;?>" alt="twitter.com"/>
 				</a>
 			<?php } ?>
 			<?php if($facebook) { ?>
-				<a class="sn" id="facebook" rel="external" href="http://facebook.com/<?php echo $user[1][0]["Facebook"];?>" title="<?php echo $user[1][0]["Facebook"];?>">
+				<a class="sn" id="facebook" rel="external" href="http://facebook.com/<?php echo $user["Facebook"];?>" title="<?php echo $user["Facebook"];?>">
 					<img src="<?php echo $facebook;?>" alt="facebook.com"/>
 				</a>
 			<?php } ?>
 			<?php if($linkedin) { ?>
-				<a class="sn" id="linkedin" rel="external" href="http://linkedin.com/<?php echo $user[1][0]["Linkedin"];?>" title="<?php echo $user[1][0]["Linkedin"];?>">
+				<a class="sn" id="linkedin" rel="external" href="http://linkedin.com/<?php echo $user["Linkedin"];?>" title="<?php echo $user["Linkedin"];?>">
 					<img src="<?php echo $linkedin;?>" alt="linkedin.com"/>
 				</a>
 			<?php } ?>
 			<?php if($google) { ?>
-				<a class="sn" id="google" href="http://plus.google.com/<?php echo $user[1][0]["Google"];?>/about" rel="external" title="<?php echo $user[1][0]["Google"];?>">
+				<a class="sn" id="google" href="http://plus.google.com/<?php echo $user["Google"];?>/about" rel="external" title="<?php echo $user["Google"];?>">
 					<img src="<?php echo $google;?>" alt="plus.google.com"/>
 				</a>
 			<?php } ?>
