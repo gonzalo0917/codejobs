09744cb28be8adea04f6caef28444d879cc463f1
Jhon <yonclev@gmail.com>
2012-10-27 19:22:07 -0500

    #47 (1) Editar perfil [IN PROGRESS]

--- www/lib/themes/newcodejobs/left.php
+++ www/lib/themes/newcodejobs/left.php
@@ -2,11 +2,13 @@
 	if(defined("_showLeft")) {
 ?>
 		<aside class="left">
-		    <ul class="nav nav-list">
-			    <li class="nav-header">List header</li>
-			    <li class="active"><a href="#">Home</a></li>
-			    <li><a href="#">Library</a></li>
-		    </ul>
+			<div class="well" style="padding: 8px 0px">
+			    <ul class="nav nav-list">
+				    <li class="nav-header">List header</li>
+				    <li class="active"><a href="#">Home</a></li>
+				    <li><a href="#">Library</a></li>
+			    </ul>
+			</div>
 		</aside>
 <?php
 	}
