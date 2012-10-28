8c76ab6285a5c530328a006d1479a22694b5e1bb
Jhon <yonclev@gmail.com>
2012-10-27 20:35:13 -0500

    #47 (1) Editar perfil [IN PROGRESS]

--- www/applications/users/views/edit_profile.php
+++ www/applications/users/views/edit_profile.php
@@ -4,6 +4,17 @@
 	echo div("edit-profile", "class");
 		echo formOpen($href, "form-add", "form-add");
 			echo p(__("Edit profile"), "resalt");
+
+			echo isset($alert) ? $alert : NULL;
+
+			echo formInput(array(	
+                "name" 	=> "name", 
+                "class" => "field-title",
+                "field" => __("Full name"), 
+                "p" 	=> TRUE, 
+                "value" => "",
+                "autofocus" => "autofocus"
+			));
 		echo formClose();
 	echo div(FALSE);
 ?>
