4c04dd308490593d166784817240fd30f0dae068
Jhon <yonclev@gmail.com>
2012-10-27 19:40:36 -0500

    #47 (1) Editar perfil [IN PROGRESS]

--- www/lib/themes/newcodejobs/left.php
+++ www/lib/themes/newcodejobs/left.php
@@ -4,7 +4,6 @@
 		<aside class="left">
 			<div class="well" style="padding: 8px 0px">
 			    <ul class="nav nav-list">
-				    <li class="nav-header">List header</li>
 				    <li class="active"><a href="#">Home</a></li>
 				    <li><a href="#">Library</a></li>
 			    </ul>
