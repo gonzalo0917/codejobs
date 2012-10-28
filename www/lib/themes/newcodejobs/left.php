e1c79d8e2e7cbae6871abed05d265519956ec9c8
Jhon <yonclev@gmail.com>
2012-10-27 20:06:44 -0500

    #47 (1) Editar perfil [IN PROGRESS]

--- www/lib/themes/newcodejobs/left.php
+++ www/lib/themes/newcodejobs/left.php
@@ -4,8 +4,24 @@
 		<aside class="left">
 			<div class="well" style="padding: 8px 0px">
 			    <ul class="nav nav-list">
-				    <li class="active"><a href="#">Home</a></li>
-				    <li><a href="#">Library</a></li>
+				    <li class="active">
+				    	<a href="#"><?php echo __("Edit profile"); ?></a>
+				    </li>
+				    <li>
+				    	<a href="#"><?php echo __("Change avatar");?></a>
+				    </li>
+				    <li>
+				    	<a href="#"><?php echo __("Change password");?></a>
+				    </li>
+				    <li>
+				    	<a href="#"><?php echo __("Options");?></a>
+				    </li>
+				    <li>
+				    	<a href="#"><?php echo __("Privacity");?></a>
+				    </li>
+				    <li>
+				    	<a href="#"><?php echo __("Social networks");?></a>
+				    </li>
 			    </ul>
 			</div>
 		</aside>
