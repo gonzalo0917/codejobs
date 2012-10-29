5dee46729137d3a4e8d5b79ea9bd91f3ef6f5c11
Carlos Santana <carlos@milkzoft.com>
2012-10-29 14:08:57 -0600

    maxLimit increase to 50

--- www/applications/cpanel/config/cpanel.php
+++ www/applications/cpanel/config/cpanel.php
@@ -11,7 +11,7 @@ define("_color2", "#DDDDDD");
 define("_color3", "#FFCCCC");
 define("_color4", "#FFAAAA");
 define("_color5", "#BBBBBB");
-define("_maxLimit", 10);
+define("_maxLimit", 50);
 
 define("_muralSize", "960x300px");
 define("_muralWidth", 960);
