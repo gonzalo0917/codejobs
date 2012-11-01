59e050ceae370f26e570852a56112324d5b3ab5a
Jhon <yonclev@gmail.com>
2012-10-31 19:20:21 -0500

    Bookmarks cache fixed

--- www/applications/bookmarks/models/bookmarks.php
+++ www/applications/bookmarks/models/bookmarks.php
@@ -141,8 +141,10 @@ class Bookmarks_Model extends ZP_Model {
 	
 	private function save() {
 		if($this->Db->insert($this->table, $this->data)) {
+			$this->Cache = $this->core("Cache");	
+			$this->Cache->removeAll("bookmarks");
+
 			$this->Users_Model = $this->model("Users_Model");
-			
 			$this->Users_Model->setCredits(1, 9);
 
 			return getAlert(__("The bookmark has been saved correctly"), "success");	
