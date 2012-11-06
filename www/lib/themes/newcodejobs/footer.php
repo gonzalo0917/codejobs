e85bfc19b2f015eeb65e80dd6121ae166c97176b
Carlos Santana <carlos@milkzoft.com>
2012-11-06 11:54:26 -0600

    Statcounter fixed

--- www/lib/themes/newcodejobs/footer.php
+++ www/lib/themes/newcodejobs/footer.php
@@ -22,18 +22,13 @@
 
         <?php echo $this->getJs(); ?>
 
-        <?php
-            if(_get("production")) {
-        ?>
-                <script type="text/javascript">
-                    var sc_project = 7655788; 
-                    var sc_invisible = 1; 
-                    var sc_security = "f167f55b"; 
-                </script>
 
-                <script type="text/javascript" src="http://www.statcounter.com/counter/counter.js"></script>
-        <?php
-            }
-        ?>
+        <script type="text/javascript">
+            var sc_project = 7655788; 
+            var sc_invisible = 1; 
+            var sc_security = "f167f55b"; 
+        </script>
+
+        <script type="text/javascript" src="http://www.statcounter.com/counter/counter.js"></script>
     </body>
 </html>
