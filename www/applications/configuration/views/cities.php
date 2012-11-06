91797af5f52a92551f053148415197c2f6d2bc96
Jhon <yonclev@gmail.com>
2012-11-05 20:25:11 -0500

    #47 (1) Editar perfil [IN PROGRESS]

--- www/applications/configuration/views/cities.php
+++ www/applications/configuration/views/cities.php
@@ -1,4 +1,8 @@
 <?php
+	if(!defined("_access")) {
+		die("Error: You don't have permission to access here..."); 
+	}
+	
 	header("Content-type: application/json");
 
 	foreach ($data as $district) {
