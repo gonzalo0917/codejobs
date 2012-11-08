b8e7543da70d17e10eece23abf1d7a2fc59fe70e
Carlos Santana <carlos@milkzoft.com>
2012-11-08 17:50:32 -0600

    Polls lastURL fixed

--- www/applications/polls/views/poll.php
+++ www/applications/polls/views/poll.php
@@ -105,7 +105,25 @@
 </div>
 <br />
 
-<?php
+<?php 
+	if(_get("domain")) {
+	?>
+		<p>
+			<script type="text/javascript"><!--
+				google_ad_client = "ca-pub-4006994369722584";
+				/* CodeJobs.biz */
+				google_ad_slot = "1672839256";
+				google_ad_width = 728;
+				google_ad_height = 90;
+				//-->
+				</script>
+				<script type="text/javascript"
+				src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+			</script>
+		</p>
+	<?php
+	}
+					
 	if(isset($special)) {
 	?>
 		<div class="fb-comments" data-href="<?php echo $URL; ?>" data-num-posts="2" data-width="750"></div>
