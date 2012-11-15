428145cb27042b9e493dd78f0a83c2d39baac1e5
Carlos Santana <carlos@milkzoft.com>
2012-11-14 23:38:38 -0600

    Display function added to footer and right

--- www/lib/themes/newcodejobs/footer.php
+++ www/lib/themes/newcodejobs/footer.php
@@ -3,16 +3,12 @@
         <footer>
             <p>
                 <?php 
-                    if(whichLanguage() === "Spanish") {
-                    ?>
-                        <a href="<?php echo path(slug(__("Advertising")), FALSE, FALSE); ?>"><?php echo __("Advertising"); ?></a> &nbsp;&nbsp;
-                        <!--<a href="<?php echo path(slug(__("Legal notice")), FALSE, FALSE); ?>"><?php echo __("Legal notice"); ?></a>  &nbsp;&nbsp;-->
-                        <!--<a href="<?php echo path(slug(__("Terms of Use")), FALSE, FALSE); ?>"><?php echo __("Terms of Use"); ?></a>  &nbsp;&nbsp;-->
-                        <!--<a href="<?php echo path(slug(__("About CodeJobs")), FALSE, FALSE); ?>"><?php echo __("About CodeJobs"); ?></a> &nbsp;&nbsp;-->
-                        <a href="<?php echo path("links", TRUE); ?>"><?php echo __("Links"); ?></a> &nbsp;&nbsp;
-                		<a href="<?php echo path("feedback"); ?>"><?php echo __("Contact us"); ?></a><br />
-                    <?php
-                    }
+                    echo display('<a href="'. path(slug(__("Advertising")), FALSE, FALSE) .'">'. __("Advertising") .'</a> &nbsp;&nbsp;
+                        <!--<a href="'. path(slug(__("Legal notice")), FALSE, FALSE) .'">'. __("Legal notice") .'</a>  &nbsp;&nbsp;-->
+                        <!--<a href="'. path(slug(__("Terms of Use")), FALSE, FALSE) .'">'. __("Terms of Use") .'</a>  &nbsp;&nbsp;-->
+                        <!--<a href="'. path(slug(__("About CodeJobs")), FALSE, FALSE) .'">'. __("About CodeJobs") .'</a> &nbsp;&nbsp;-->
+                        <a href="'. path("links", TRUE) .'">'. __("Links") .'</a> &nbsp;&nbsp;
+                        <a href="'. path("feedback") .'">'. __("Contact us") .'</a><br />', TRUE, "Spanish");
                     
                     echo __("This site is licensed under a"); ?> 
                     <a href="http://creativecommons.org/licenses/by/3.0/" target="_blank">Creative Commons Attribution 3.0 License</a>. 
@@ -22,13 +18,14 @@
 
         <?php echo $this->getJs(); ?>
 
+        <?php
+            echo display('<script type="text/javascript">
+                            var sc_project = 7655788; 
+                            var sc_invisible = 1; 
+                            var sc_security = "f167f55b"; 
+                        </script> 
 
-        <script type="text/javascript">
-            var sc_project = 7655788; 
-            var sc_invisible = 1; 
-            var sc_security = "f167f55b"; 
-        </script>
-
-        <script type="text/javascript" src="http://www.statcounter.com/counter/counter.js"></script>
+                        <script type="text/javascript" src="http://www.statcounter.com/counter/counter.js"></script>', 4);
+        ?>
     </body>
 </html>
