fafc0753ed988c475d8fbeea8a9c057c53b1e784
Carlos Santana <carlos@milkzoft.com>
2012-11-28 12:55:34 -0600

    Addthis fixed

--- www/applications/blog/views/posts.php
+++ www/applications/blog/views/posts.php
@@ -46,10 +46,10 @@ if(is_array($posts)) {
 			<div class="post-content">
 				<div class="social" style="position: relative; z-index:100;">
 					<!-- AddThis Button BEGIN -->
-						<div class="addthis_toolbox addthis_default_style">
-							<a class="addthis_button_facebook_like" fb:like:layout="button_count" fb:like:href="<?php echo $URL; ?>" fb:like:title="<?php echo stripslashes($post["Title"]); ?>"></a>
-							<a class="addthis_button_tweet" tw:via="codejobs" addthis:title="<?php echo stripslashes($post["Title"]); ?>" tw:url="<?php echo $URL; ?>"></a>
-							<a class="addthis_button_google_plusone_badge" g:plusone:title="<?php echo stripslashes($post["Title"]); ?>" g:plusone:href="<?php echo $URL; ?>"></a>
+						<div class="addthis_toolbox addthis_default_style" addthis:url="<?php echo $URL; ?>" addthis:title="<?php echo stripslashes($post["Title"]); ?>">
+							<a class="addthis_button_facebook_like"></a>
+							<a class="addthis_button_tweet" tw:via="codejobs"></a>
+							<a class="addthis_button_google_plusone_badge"></a>
 						</div>
 						<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
 						<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-50b64f6b39227d84"></script>
