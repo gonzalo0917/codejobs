8ef2de40ac9fd840226edb5c8531a6566c066b77
Jhon <yonclev@gmail.com>
2012-11-28 19:31:49 -0500

    #92 Solucionando mensaje de 'Ya has votado en esta encuesta'

--- www/applications/polls/views/js/poll.js
+++ www/applications/polls/views/js/poll.js
@@ -1,4 +1,7 @@
+var loading = false;
+
 $("#send-vote").click(function(e) {
+	e.preventDefault();
 	var found = false;
 	$("#polls input[type='radio']").each(function() {
 		if (this.checked) found = this.value;
@@ -7,10 +10,14 @@ $("#send-vote").click(function(e) {
 	if (found) {
 		$("#polls input[type='button']").fadeOut(function() {
 			$("#warningGradientOuterBarG").fadeIn(function() {
-				$(poll_selector).load(PATH + "/polls/vote/", {
-					"answer": found,
-					"ID_Poll": $("input[name='ID_Poll']").val()
-				});
+				if (!loading) {
+					loading = true;
+
+					$(poll_selector).load(PATH + "/polls/vote/", {
+						"answer": found,
+						"ID_Poll": $("input[name='ID_Poll']").val()
+					}, new Function("loading = false"));
+				}
 			});
 		});
 	} else {
