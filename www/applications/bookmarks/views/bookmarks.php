b19e6c53e74d776b0682920ac4364523c068f9fe
Carlos Santana <carlos@milkzoft.com>
2012-12-21 17:26:03 -0600

    Rand fixed

--- www/applications/bookmarks/views/bookmarks.php
+++ www/applications/bookmarks/views/bookmarks.php
@@ -55,19 +55,19 @@
 			?>
 			
 			<?php					
-				if($i === $rand2) {
+				if($i === $rand2) { 
 					echo display('<p>
-                        <script type="text/javascript">
-                            google_ad_client = "ca-pub-4006994369722584";
-                            /* CodeJobs.biz */
-                            google_ad_slot = "1672839256";
-                            google_ad_width = 728;
-                            google_ad_height = 90;
-                            </script>
-                            <script type="text/javascript"
-                            src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-                        </script>
-                    </p>', 4);
+									<script type="text/javascript">
+										google_ad_client = "ca-pub-4006994369722584";
+										/* CodeJobs.biz */
+										google_ad_slot = "1672839256";
+										google_ad_width = 728;
+										google_ad_height = 90;
+										</script>
+										<script type="text/javascript"
+										src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+									</script>
+								</p>', 4);
 				}
 
 				$i++;
