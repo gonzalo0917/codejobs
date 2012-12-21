f166537e2a191799e08e337ccd6e3f003448ac4c
Carlos Santana <carlos@milkzoft.com>
2012-12-21 16:13:59 -0600

    Rand on bookmarks fixed

--- www/applications/bookmarks/views/bookmarks.php
+++ www/applications/bookmarks/views/bookmarks.php
@@ -7,8 +7,8 @@
 <div class="bookmarks">
 	<?php 
 		$i = 1;
-		$rand1 = rand(1, 10);
-		$rand2 = rand(11, 20);
+		$rand1 = rand(1, 5);
+		$rand2 = rand(5, 10);
 		
 		foreach($bookmarks as $bookmark) { 
 			$URL = path("bookmarks/". $bookmark["ID_Bookmark"] ."/". $bookmark["Slug"], FALSE, $bookmark["Language"]);
