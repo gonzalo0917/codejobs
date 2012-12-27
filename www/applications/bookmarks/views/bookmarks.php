4efaac249a90963f34e4d428cc58caa6f64c06ce
Carlos Santana <carlos@milkzoft.com>
2012-12-27 17:03:14 -0600

    Bookmarks fixed

--- www/applications/bookmarks/views/bookmarks.php
+++ www/applications/bookmarks/views/bookmarks.php
@@ -35,9 +35,7 @@
 			<?php echo display(social($URL, $bookmark["Title"], FALSE), 4); ?>	
 
 			<p class="justify">				
-				<?php 
-					echo stripslashes(compress($bookmark["Description"])); 
-				?>
+				<?php echo stripslashes(compress($bookmark["Description"])); ?>
 			</p>
 
 			<br />
@@ -50,23 +48,22 @@
 					</p>
 			<?php 
 				} 
-			?>
-			
-			<?php					
-				if($i === 5) { 
-					echo display('<p>
-									<script type="text/javascript">
-										google_ad_client = "ca-pub-4006994369722584";
-										/* CodeJobs.biz */
-										google_ad_slot = "1672839256";
-										google_ad_width = 728;
-										google_ad_height = 90;
-										</script>
-										<script type="text/javascript"
-										src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-									</script>
-								</p>', 4);
+					
+				if($i == 5) { echo $i;
+				?>
+					<p>
+						<script type="text/javascript">
+							google_ad_client = "ca-pub-4006994369722584";
+							/* CodeJobs.biz */
+							google_ad_slot = "1672839256";
+							google_ad_width = 728;
+							google_ad_height = 90;
+						</script>
+
+						<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+					</p>
 				}
+				<?php
 
 				$i++;
 			?>			
