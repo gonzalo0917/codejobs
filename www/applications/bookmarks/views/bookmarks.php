4247567088db41a0cc08189bde9181f29127c457
Carlos Santana <carlos@milkzoft.com>
2012-12-27 17:05:05 -0600

    Bookmarks

--- www/applications/bookmarks/views/bookmarks.php
+++ www/applications/bookmarks/views/bookmarks.php
@@ -48,25 +48,20 @@
 					</p>
 			<?php 
 				} 
+			?>
 					
-				if($i == 5) { echo $i;
-				?>
-					<p>
-						<script type="text/javascript">
-							google_ad_client = "ca-pub-4006994369722584";
-							/* CodeJobs.biz */
-							google_ad_slot = "1672839256";
-							google_ad_width = 728;
-							google_ad_height = 90;
-						</script>
+			<p>
+				<script type="text/javascript">
+					google_ad_client = "ca-pub-4006994369722584";
+					/* CodeJobs.biz */
+					google_ad_slot = "1672839256";
+					google_ad_width = 728;
+					google_ad_height = 90;
+				</script>
 
-						<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
-					</p>
-				}
-				<?php
-
-				$i++;
-			?>			
+				<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+			</p>
+							
 			<br />
 	<?php 
 		} 
