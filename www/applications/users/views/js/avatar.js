9c4153ca8fd1dbac5a2bb8e0067d33ad39ce45e0
Jhon <yonclev@gmail.com>
2013-01-08 20:35:04 -0500

    #47 Formulario 'Avatar' [IN PROGRESS]

--- www/applications/users/views/js/avatar.js
+++ www/applications/users/views/js/avatar.js
@@ -13,8 +13,6 @@
 			
 			if (/image/i .test(file.type)) {
 				previewImage(file);
-
-				window.setTimeout(markImage, 500);
 			} else {
 				alert("Image type does not supported");
 			}
@@ -27,6 +25,8 @@
 
 			reader.onload = function (event) {
 				$("#avatar-image").attr("src", event.target.result);
+
+				window.setTimeout(markImage, 0);
 			}
 
 			reader.readAsDataURL(file);
@@ -42,24 +42,20 @@
 			height = $("#avatar-image").height(),
 			small  = (width <= 90 && height <= 90),
 			square = (width === height);
-console.log("******** SMALL = " + (small ? "Si" : "No"));
+
 		if(!square || !small) {
 			if(square) {
-				console.log("Square");
 				$("#marker").width(height - 6).height(height - 6).css({top: "10px", left: "10px"});
 			} else if(width > height) {
-				console.log("Width mayor");
 				var pos_left = parseInt((width - height)/2) + 10;
 				$("#marker").width(height - 6).height(height - 6).css({top: "10px", left: pos_left + "px"});
 			} else {
-				console.log("Height mayor");
 				var pos_top = parseInt((height - width)/2) + 10;
 				$("#marker").width(width - 6).height(width - 6).css({top: pos_top + "px", left: "10px"});
 			}
 
 			$("#marker").css("display", "block");
 		} else {
-			console.log("Ocultar");
 			$("#marker").hide();
 		}
 	}
