b2145b7c428d7cdcac769baae5e501391c219328
Carlos Santana <carlos@milkzoft.com>
2013-01-17 16:00:49 -0600

    Header modified

--- www/lib/themes/newcodejobs/min/header.php
+++ www/lib/themes/newcodejobs/min/header.php
@@ -102,11 +102,11 @@
 
 							<input name="login" class="login-submit" type="submit" value="<?php echo __("Login"); ?>" />		 					
 							<?php 
-								if(in_array(getIP(), _get("allowIP"))) {
+								if(isAllowedIP()) {
 								?>
 									<br />
 									<br />
-									<a href="<?php echo path("users/facebook/login"); ?>" title="<?php echo __("Sign in with Facebook"); ?>"><img src="<?php echo path("www/applications/users/views/images/login/facebook_$lang.png", TRUE); ?>" alt="<?php echo __("Sign in with Facebook"); ?>" class="no-border" /><br /><a href="#" title="<?php echo __("Sign in with Twitter"); ?>"><img src="<?php echo path("www/applications/users/views/images/login/twitter_$lang.png", TRUE); ?>" alt="<?php echo __("Sign in with Twitter"); ?>" class="no-border" /></a>							
+									<a href="<?php echo path("users/service/facebook/login"); ?>" title="<?php echo __("Sign in with Facebook"); ?>"><img src="<?php echo path("www/applications/users/views/images/login/facebook_$lang.png", TRUE); ?>" alt="<?php echo __("Sign in with Facebook"); ?>" class="no-border" /><br /><a href="#" title="<?php echo __("Sign in with Twitter"); ?>"><img src="<?php echo path("www/applications/users/views/images/login/twitter_$lang.png", TRUE); ?>" alt="<?php echo __("Sign in with Twitter"); ?>" class="no-border" /></a>							
 								<?php
 								}
 							?>							
