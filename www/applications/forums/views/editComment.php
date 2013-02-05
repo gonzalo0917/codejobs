c612c4188f3cd3976fe117f48cc7655784877479
jaime <jaime.victoria.90@gmail.com>
2013-02-04 18:42:45 -0600

    Forums edit comments new view

---
+++ www/applications/forums/views/editComment.php
@@ -0,0 +1,23 @@
+<?php 
+if(!defined("_access")) { 
+	die("Error: You don't have permission to access here..."); 
+}
+	$fid     = isset($data) ? recoverPOST("fid", $data[0]["ID_Parent"]) : recoverPOST("fid");
+	$pid     = isset($data) ? recoverPOST("pid", $data[0]["ID_Post"]) : recoverPOST("pid");
+	$content = isset($data) ? recoverPOST("content", $data[0]["Content"]) : recoverPOST("content");
+?>
+	<p id="fmessage"></p>
+	<div class="post-title">
+		<span class="forums-create"><?php echo __("Edit Comment"); ?></span>	
+		<br />
+		<form action="#" method="post">
+			<textarea id="pcontent" name="content" placeholder="Write the content here..." class="span8 forums-textarea" rows= "5"><?php echo $content ?></textarea> <br />
+			<input id="cedit" class="btn btn-success" name="publish" type="button" value="<?php echo __("Edit"); ?>" />
+			<input id="pcancel" class="btn btn-danger" name="cancel" type="button" value="<?php echo __("Cancel"); ?>" />
+
+			<input id="pid" name="pid" type="hidden" value="<?php echo $pid; ?>" />
+			<input id="fid" name="fid" type="hidden" value="<?php echo $fid; ?>" />
+			<input id="fname" type="hidden" value="<?php echo $forum ?>" />
+			<input id="needcontent" type="hidden" value="<?php echo __("Content must have at least 90 characters..."); ?>" />
+		</form>
+	</div>
