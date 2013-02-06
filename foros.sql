5c399bf755a86676e0a8393d0310e00a85bb851e
jaime <jaime.victoria.90@gmail.com>
2013-02-05 20:49:08 -0600

    Forums add forums database

---
+++ foros.sql
@@ -0,0 +1,84 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 05-02-2013 a las 20:46:03
+-- Versión del servidor: 5.5.29
+-- Versión de PHP: 5.3.10-1ubuntu3.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `codejobs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `muu_forums`
+--
+
+CREATE TABLE IF NOT EXISTS `muu_forums` (
+  `ID_Forum` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `Title` varchar(120) NOT NULL,
+  `Slug` varchar(120) NOT NULL,
+  `Description` varchar(250) NOT NULL,
+  `Topics` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `Replies` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `Last_Reply` int(11) unsigned NOT NULL DEFAULT '0',
+  `Last_Date` varchar(50) NOT NULL,
+  `Language` varchar(20) NOT NULL DEFAULT 'Spanish',
+  `Situation` varchar(15) NOT NULL DEFAULT 'Active',
+  PRIMARY KEY (`ID_Forum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `muu_forums_posts`
+--
+
+CREATE TABLE IF NOT EXISTS `muu_forums_posts` (
+  `ID_Post` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `ID_User` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `ID_Forum` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `ID_Parent` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `Forum_Name` varchar(100) NOT NULL,
+  `Title` varchar(150) NOT NULL,
+  `Slug` varchar(150) NOT NULL,
+  `Content` text NOT NULL,
+  `Author` varchar(50) NOT NULL,
+  `Start_Date` int(11) unsigned NOT NULL DEFAULT '0',
+  `Text_Date` varchar(50) NOT NULL,
+  `Hour` varchar(15) NOT NULL DEFAULT '00:00:00',
+  `Visits` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `Topic` tinyint(1) NOT NULL DEFAULT '0',
+  `Tags` varchar(150) NOT NULL,
+  `Language` varchar(20) NOT NULL DEFAULT 'English',
+  `Situation` varchar(15) NOT NULL DEFAULT 'Active',
+  PRIMARY KEY (`ID_Post`),
+  KEY `ID_User` (`ID_User`),
+  KEY `ID_Forum` (`ID_Forum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `muu_forums_posts`
+--
+ALTER TABLE `muu_forums_posts`
+  ADD CONSTRAINT `muu_forums_posts_ibfk_1` FOREIGN KEY (`ID_User`) REFERENCES `muu_users` (`ID_User`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
