73cb23597a4dd4f4a2b83831812d5dbdb99ad094
Carlos Santana <carlos@milkzoft.com>
2013-02-12 23:29:22 -0600

    Google Analitycs

--- www/lib/themes/newcodejobs/footer.php
+++ www/lib/themes/newcodejobs/footer.php
@@ -64,6 +64,7 @@
                 <?php
                 }
 
+                echo display('<script type="text/javascript">var _gaq = _gaq || [];_gaq.push([\'_setAccount\', \'UA-38459206-1\']);_gaq.push([\'_trackPageview\']);(function() {var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s);})();</script>', 4);
                 echo display('<script type="text/javascript">var sc_project = 7655788;var sc_invisible = 1;var sc_security = "f167f55b";</script><script type="text/javascript" src="http://www.statcounter.com/counter/counter.js"></script>', 4);
                 echo display('<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>', 4);
                 echo display('<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>', 4);
