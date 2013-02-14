9d55005707e1ea5b27b92222edd4fee38bfaf6ed
Edgardo Ramírez <soldiercrp@gmail.com>
2013-02-13 18:26:42 -0600

    Polls standard PSR updated

--- www/applications/polls/controllers/cpanel.php
+++ www/applications/polls/controllers/cpanel.php
@@ -6,29 +6,32 @@ if (!defined("ACCESS")) {
 	die("Error: You don't have permission to access here...");
 }
 
-class CPanel_Controller extends ZP_Load {
-	
+class CPanel_Controller extends ZP_Load
+{
+
 	private $vars = array();
-	
-	public function __construct() {		
+
+	public function __construct()
+	{
 		$this->app("cpanel");
-		
+
 		$this->application = whichApplication();
-		
+
 		$this->CPanel = $this->classes("cpanel", "CPanel", null, "cpanel");
-		
+
 		$this->isAdmin = $this->CPanel->load();
-		
+
 		$this->vars = $this->CPanel->notifications();
-		
+
 		$this->CPanel_Model = $this->model("CPanel_Model");
-		
+
 		$this->Templates = $this->core("Templates");
-		
+
 		$this->Templates->theme("cpanel");
 	}
-	
-	public function index() {
+
+	public function index()
+	{
 		if ($this->isAdmin) {
 			redirect("cpanel");
 		} else {
@@ -36,7 +39,8 @@ class CPanel_Controller extends ZP_Load {
 		}
 	}
 
-	public function check() {
+	public function check()
+	{
 		if (POST("trash") and is_array(POST("records"))) { 
 			foreach (POST("records") as $record) {
 				$this->trash($record, true); 
@@ -60,11 +64,12 @@ class CPanel_Controller extends ZP_Load {
 		return false;
 	}
 
-	public function delete($ID = 0, $return = false) {
+	public function delete($ID = 0, $return = false)
+	{
 		if (!$this->isAdmin) {
 			$this->login();
 		}
-		
+
 		if ($this->CPanel_Model->delete($ID)) {
 			if ($return) {
 				return true;
@@ -77,14 +82,15 @@ class CPanel_Controller extends ZP_Load {
 			}
 
 			redirect("$this->application/cpanel/results");
-		}	
+		}
 	}
 
-	public function restore($ID = 0, $return = false) { 
+	public function restore($ID = 0, $return = false)
+	{ 
 		if (!$this->isAdmin) {
 			$this->login();
 		}
-		
+
 		if ($this->CPanel_Model->restore($ID)) {
 			if ($return) {
 				return true;
@@ -100,15 +106,16 @@ class CPanel_Controller extends ZP_Load {
 		}
 	}
 
-	public function trash($ID = 0, $return = false) {
+	public function trash($ID = 0, $return = false)
+	{
 		if (!$this->isAdmin) {
 			$this->login();
 		}
 		
-		if ($this->CPanel_Model->trash($ID)) {		
+		if ($this->CPanel_Model->trash($ID)) {
 			if ($return) {
 				return true;
-			}	
+			}
 
 			redirect("$this->application/cpanel/results");
 		} else {
@@ -119,24 +126,25 @@ class CPanel_Controller extends ZP_Load {
 			redirect("$this->application/cpanel/add");
 		}
 	}
-	
-	public function add() {
+
+	public function add()
+	{
 		if (!$this->isAdmin) {
 			$this->login();
 		}
-		
+
 		$this->title("Add");
-		
-		$this->js("add", "polls");	
-		
+
+		$this->js("add", "polls");
+
 		$this->helper("forms");
 
 		$this->CSS("forms", "cpanel");
-				
+
 		$Model = ucfirst($this->application) ."_Model";
-		
+
 		$this->$Model = $this->model($Model);
-		
+
 		if (POST("save")) {
 			$this->vars["alert"] = $this->$Model->cpanel("save");
 		} elseif (POST("cancel")) {
@@ -147,35 +155,36 @@ class CPanel_Controller extends ZP_Load {
 		
 		$this->render("content", $this->vars);
 	}
-		
-	public function edit($ID = 0) {
+
+	public function edit($ID = 0)
+	{
 		if (!$this->isAdmin) {
 			$this->login();
 		}
-		
+
 		if ((int) $ID === 0) { 
 			redirect($this->application ."/cpanel/results");
 		}
 
 		$this->title("Edit");
-		
+
 		$this->CSS("forms", "cpanel");
-		
+
 		$Model = ucfirst($this->application) ."_Model";
-		
+
 		$this->$Model = $this->model($Model);
-		
+
 		if (POST("edit")) {
 			$this->vars["alert"] = $this->$Model->cpanel("edit");
 		} elseif (POST("cancel")) {
 			redirect("cpanel");
 		} 
-		
+
 		$data = $this->$Model->getByID($ID);
-		
+
 		if ($data) {
 			$this->helper("forms");
-			
+
 			$this->vars["data"] = $data;
 			$this->vars["view"] = $this->view("add", true, $this->application);
 			
@@ -184,49 +193,51 @@ class CPanel_Controller extends ZP_Load {
 			redirect($this->application ."/cpanel/results");
 		}
 	}
-	
-	public function login() {
+
+	public function login()
+	{
 		$this->title("Login");
 		$this->CSS("login", "users");
-		
-		if (POST("connect")) {	
+
+		if (POST("connect")) {
 			$this->Users_Controller = $this->controller("Users_Controller");
-			
+
 			$this->Users_Controller->login("cpanel");
 		} else {
-			$this->vars["URL"]  = getURL();
+			$this->vars["URL"] = getURL();
 			$this->vars["view"] = $this->view("login", true, "cpanel");
 		}
-		
+
 		$this->render("include", $this->vars);
 		$this->rendering("header", "footer");
-		
+
 		exit;
 	}
 		
-	public function results() {
+	public function results()
+	{
 		if (!$this->isAdmin) {
 			$this->login();
 		}
 
 		$this->check();
-		
+
 		$this->title("Manage ". ucfirst($this->application));
 
 		$this->CSS("results", "cpanel");
 		$this->CSS("pagination");
-		
+
 		$this->js("checkbox");
-			
+
 		$trash = (segment(3, isLang()) === "trash") ? true : false;
 		
-		$this->vars["total"] 	  = $this->CPanel_Model->total($trash); 
-		$this->vars["tFoot"] 	  = $this->CPanel_Model->records($trash);
-		$this->vars["message"]    = (!$this->vars["tFoot"]) ? "Error" : null;
+		$this->vars["total"] = $this->CPanel_Model->total($trash); 
+		$this->vars["tFoot"] = $this->CPanel_Model->records($trash);
+		$this->vars["message"] = (!$this->vars["tFoot"]) ? "Error" : null;
 		$this->vars["pagination"] = $this->CPanel_Model->getPagination($trash);
-		$this->vars["trash"]  	  = $trash;	
-		$this->vars["search"] 	  = getSearch(); 			
-		$this->vars["view"]       = $this->view("results", true, $this->application);
+		$this->vars["trash"] = $trash;
+		$this->vars["search"] = getSearch();
+		$this->vars["view"] = $this->view("results", true, $this->application);
 		
 		$this->render("content", $this->vars);
 	}
--- www/applications/polls/views/add.php
+++ www/applications/polls/views/add.php
@@ -3,13 +3,13 @@
 		die("Error: You don't have permission to access here..."); 
 	}
 
-	$ID  	   = isset($data) ? recoverPOST("ID", $data["question"]["ID_Poll"])			 : recoverPOST("ID");
-	$title     = isset($data) ? recoverPOST("title", $data["question"]["Title"])		 : recoverPOST("title");
-	$answers   = isset($data) ? recoverPOST("answers", $data["answers"])				 : recoverPOST("answers");
+	$ID = isset($data) ? recoverPOST("ID", $data["question"]["ID_Poll"]) : recoverPOST("ID");
+	$title = isset($data) ? recoverPOST("title", $data["question"]["Title"]) : recoverPOST("title");
+	$answers = isset($data) ? recoverPOST("answers", $data["answers"]) : recoverPOST("answers");
 	$situation = isset($data) ? recoverPOST("situation", $data["question"]["Situation"]) : recoverPOST("situation");
-	$language  = isset($data) ? recoverPOST("language", $data["question"]["Language"])   : recoverPOST("language");
-	$action	   = isset($data) ? "edit"											 		 : "save";
-	$href	   = isset($data) ? path("polls/cpanel/$action/$ID/") 						 : path("polls/cpanel/add/");
+	$language = isset($data) ? recoverPOST("language", $data["question"]["Language"]) : recoverPOST("language");
+	$action = isset($data) ? "edit" : "save";
+	$href = isset($data) ? path("polls/cpanel/$action/$ID/") : path("polls/cpanel/add/");
 
 	echo div("add-form", "class");
 		echo formOpen($href, "form-add", "form-add");
@@ -17,61 +17,61 @@
 			
 			echo isset($alert) ? $alert : null;
 
-			echo formInput(array(	
-				"name" 	=> "title", 
+			echo formInput(array(
+				"name" => "title", 
 				"class" => "span10 required", 
 				"field" => __("Question"), 
-				"p" 	=> true, 
+				"p" => true, 
 				"value" => $title
 			));
-						
-			
+
+
 			echo formField(null, __("Answers") ." (". __("Empty answers not be added") . ")");
 			
 			if (is_array($answers)) { 
 				$count = 1;
 
 				foreach ($answers as $answer) { 
-					echo p(true, "field panswer");	
-						echo formInput(array("name" => "answers[]", "class" => "span10 required", "value" => $answer["Answer"]));	
+					echo p(true, "field panswer");
+						echo formInput(array("name" => "answers[]", "class" => "span10 required", "value" => $answer["Answer"]));
 					echo p(false);
 
 					$count++;
 				}
 
 				for ($i = $count; $i <= 6; $i++) {
-					echo p(true, "field panswer");	
+					echo p(true, "field panswer");
 						echo formInput(array("name" => "answers[]", "class" => "span10 required", "value" => ""));
 					echo p(false);
 				}
 			} else { 
-				echo p(true, "field panswer");	
+				echo p(true, "field panswer");
 					echo formInput(array("name" => "answers[]", "class" => "span10 required", "value" => $answers));
 					echo formInput(array("name" => "answers[]", "class" => "span10 required", "value" => $answers));
 					echo formInput(array("name" => "answers[]", "class" => "span10 required", "value" => $answers));
 					echo formInput(array("name" => "answers[]", "class" => "span10 required", "value" => $answers));
 					echo formInput(array("name" => "answers[]", "class" => "span10 required", "value" => $answers));
-					echo formInput(array("name" => "answers[]", "class" => "span10 required", "value" => $answers));	
+					echo formInput(array("name" => "answers[]", "class" => "span10 required", "value" => $answers));
 				echo p(false);
-			} 	
+			}
 
-			echo formField(null, __("Language") ."<br />". getLanguagesInput($language, "language", "select"));		
-			
+			echo formField(null, __("Language") ."<br />". getLanguagesInput($language, "language", "select"));
+
 			$options = array(
-				0 => array("value" => "Active",   "option" => __("Active"),   "selected" => ($situation === "Active")   ? true : false),
+				0 => array("value" => "Active", "option" => __("Active"), "selected" => ($situation === "Active") ? true : false),
 				1 => array("value" => "Inactive", "option" => __("Inactive"), "selected" => ($situation === "Inactive") ? true : false)
 			);
 
 			echo formSelect(array(
-				"name" 	=> "situation", 
-				"p" 	=> true, 
+				"name" => "situation", 
+				"p" => true, 
 				"class" => "required", 
 				"field" => __("Situation")), 
 				$options
 			);
-			
+
 			echo formSave($action);
-			
+
 			echo formInput(array("name" => "ID", "type" => "hidden", "value" => $ID));
 		echo formClose();
 	echo div(false);
