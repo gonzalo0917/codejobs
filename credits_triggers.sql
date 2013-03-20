2319334eebe3c43404508f890cb4220f2bfda99b
Jhon <yonclev@gmail.com>
2013-03-19 20:02:45 -0500

    #212 Trigger added for update on muu_codes [IN PROGRESS]

--- credits_triggers.sql
+++ credits_triggers.sql
@@ -44,3 +44,18 @@ CREATE TRIGGER codes_insert AFTER INSERT ON muu_codes
 		END IF;
 	END;
 %%
+
+DROP TRIGGER IF EXISTS codes_update;
+CREATE TRIGGER codes_update AFTER UPDATE ON muu_codes
+	FOR EACH ROW BEGIN
+		IF (OLD.Situation <> 'Deleted' AND OLD.Situation <> 'Draft') AND (NEW.Situation = 'Deleted' OR NEW.Situation = 'Draft') THEN
+			UPDATE muu_users SET Codes = Codes - 1, Credits = Credits - 2, Recommendation = Recommendation - 3
+			WHERE ID_User = OLD.ID_User;
+		ELSE
+			IF (OLD.Situation = 'Deleted' OR OLD.Situation = 'Draft') AND (NEW.Situation <> 'Deleted' AND NEW.Situation <> 'Draft') THEN
+				UPDATE muu_users SET Codes = Codes + 1, Credits = Credits + 2, Recommendation = Recommendation + 3
+				WHERE ID_User = OLD.ID_User;
+			END IF;
+		END IF;
+	END;
+%%
