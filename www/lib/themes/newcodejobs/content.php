de4bd1c3aa8e9a23d4e0b91a992e2daee439be07
Carlos Santana <carlos@milkzoft.com>
2013-04-08 21:50:23 -0500

    Forums fixed

--- www/lib/themes/newcodejobs/content.php
+++ www/lib/themes/newcodejobs/content.php
@@ -11,7 +11,7 @@
 	<div id="content"<?php echo $style; ?>>
 		<?php
 			if (segment(0, isLang()) === "live" or segment(0, isLang()) === "forums") {
-				echo display('<div style="width: 728px; margin-left: 120px;">'. getAd("728px") .'</div>', 4);
+				echo display('<div style="width: 728px; margin-left: 115px;">'. getAd("728px") .'</div>', 4);
 			} else {
 				echo display('<p>'. getAd("728px") .'</p>', 4);
 			}
