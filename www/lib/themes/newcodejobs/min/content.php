d8f62997db2943113559e16209b3e26deb7284f4
Carlos Santana <carlos@milkzoft.com>
2013-04-14 23:33:03 -0500

    Minificados actualizados

--- www/lib/themes/newcodejobs/min/content.php
+++ www/lib/themes/newcodejobs/min/content.php
@@ -1,1 +1,25 @@
-<?php if (segment(0, isLang())==="forums"){$style=' style="width: 1000px;"';}elseif ((segment(0, isLang())==="codes" or segment(0, isLang())==="blog") and segment(1, isLang())==="add"){$style=' style="width: 1000px;"';}else{$style=null;}?><div id="content"<?php echo $style;?>><?php if (segment(0, isLang())==="live" or segment(0, isLang()) === "forums"){echo display('<div style="width: 728px;margin-left: 120px;">'. getAd("728px") .'</div>', 4);}else{echo display('<p>'. getAd("728px") .'</p>', 4);}$this->load(isset($view) ? $view : null, true);?></div>
+	<?php 
+		$app = segment(0, isLang());
+
+		if ($app === "forums") {
+			$style = ' style="width: 1000px;"';
+		} elseif (($app === "codes" or $app === "blog") and segment(1, isLang()) === "add") {
+			$style = ' style="width: 1000px;"';
+		} else {
+			$style = null;
+		}
+
+
+		if ($app === "users") { ?>
+			<div id="contentUsers">
+		<?php } else { ?>
+			<div id="content"<?php echo $style; ?>>
+		<?php }
+			if (segment(0, isLang()) === "live" or segment(0, isLang()) === "forums") {
+				echo display('<div style="width: 728px; margin-left: 115px;">'. getAd("728px") .'</div>', 4);
+			} else {
+				echo display('<p>'. getAd("728px") .'</p>', 4);
+			}
+		
+		$this->load(isset($view) ? $view : null, true); ?>
+	</div>
--- www/lib/themes/newcodejobs/min/left.php
+++ www/lib/themes/newcodejobs/min/left.php
@@ -2,40 +2,28 @@
 	if (defined("SHOW_LEFT")) {
 		if (SESSION("ZanUser")) {
 			switch (segment(1, isLang())) {
-				case "avatar": case "password": case "options": case "social": case "email": case "cv":
+				case "password": case "options": case "cv":
 					$active = segment(1, isLang());
 					break;
 				default:
-					$active = "about";
+					$active = "cv";
 			}
 			?>
-			<aside class="left">
-				<div class="well" style="padding: 8px 0px">
-				    <ul class="nav nav-list">
-					    <li<?php echo $active === "about" ? ' class="active"' : ''; ?>>
-					    	<a href="<?php echo path("users/about/"); ?>"><?php echo __("About me"); ?></a>
+			<header class="top">
+				    <ul class="nav nav-pills">
+					    <li<?php echo $active === "cv" ? ' class="active"' : ''; ?>>
+					    	<a href="<?php echo path("users/cv/"); ?>"><?php echo __("Update Resume");?></a>
 					    </li>
 					    <li<?php echo $active === "password" ? ' class="active"' : ''; ?>>
 					    	<a href="<?php echo path("users/password/"); ?>"><?php echo __("Password");?></a>
 					    </li>
-					    <li<?php echo $active === "email" ? ' class="active"' : ''; ?>>
-					    	<a href="<?php echo path("users/email/"); ?>"><?php echo __("E-mail");?></a>
-					    </li>
-					    <li<?php echo $active === "cv" ? ' class="active"' : ''; ?>>
-					    	<a href="<?php echo path("users/cv/"); ?>"><?php echo "Curriculum Vitae";?></a>
-					    </li>
-					    <li<?php echo $active === "avatar" ? ' class="active"' : ''; ?>>
-					    	<a href="<?php echo path("users/avatar/"); ?>"><?php echo __("Avatar");?></a>
-					    </li>
-					    <li<?php echo $active === "options" ? ' class="active"' : ''; ?>>
+					    <!-- <li<?php echo $active === "options" ? ' class="active"' : ''; ?>>
 					    	<a href="<?php echo path("users/options/"); ?>"><?php echo __("Options");?></a>
-					    </li>
-					    <li<?php echo $active === "social" ? ' class="active"' : ''; ?>>
-					    	<a href="<?php echo path("users/social/"); ?>"><?php echo __("Social networks");?></a>
-					    </li>
+					    </li> -->
 				    </ul>
 				</div>
-			</aside>
+			</header>
 			<?php
 		}
 	}
+?>
