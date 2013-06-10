b9860538b915d88edb56422d268ac2dc52743ecb
Carlos <carlos@milkzoft.com>
2013-06-10 16:50:51 -0500

    Adfly added to buffer

--- www/applications/buffer/controllers/buffer.php
+++ www/applications/buffer/controllers/buffer.php
@@ -49,6 +49,14 @@ class Buffer_Controller extends ZP_Load
 					foreach ($posts as $post) {
 						$URL = path("blog/". $post["Year"] ."/". $post["Month"] ."/". $post["Day"] ."/". $post["Slug"], false, $post["Language"]);					
 						
+						$adflyAPI = 'http://api.adf.ly/api.php?key=1f623be7584b81f4513d0730a17710c&uid=4317454&advert_type=int&domain=adf.ly&url=$URL';
+
+						$adflyURL = file_get_contents($adflyAPI);
+
+						if ($adflyURL != "error") {
+							$URL = $adflyURL;
+						}
+
 						$data = array(
 							"text" 			=> stripslashes($post["Title"]) ." ". $URL ." ". _bufferVia,
 							"profile_ids[]" => $this->bufferProfiles[$i]
