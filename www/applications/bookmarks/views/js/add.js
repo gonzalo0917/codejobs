8dbed8fa445dcbe26c71d6f6e59e33b273a2f5a2
gjpv <gjpv@linux-5jy8.site>
2013-08-13 22:12:46 -0500

    #247 - Se duplican las etiquetas

--- www/applications/bookmarks/views/js/add.js
+++ www/applications/bookmarks/views/js/add.js
@@ -31,8 +31,47 @@
 		$("input[name='URL']").removeClass("loading");
 		$("input[name='title']").val(data.Title);
 		$("textarea[name='description']").val(data.Description);
-		$("input[name='tags']").val(data.Keywords);
+		$("input[name=tags]").val(data.Keywords);
 
 		requesting = false;
 	}
+
+	$('input[name=tags]').focusout(function(event){
+		if($(this).val()!=''){
+			var tags = $(this).val().split(',');
+			if(tags.length >2){
+				var tags_txt = '';
+				tags = unique( tags );
+				for(var i in tags ){
+					tags_txt+= tags[i] + ( (tags.length-1) ==i?'':',' );
+				}
+				$(this).val(tags_txt);
+			}
+		}
+	});
+
+	function unique(ar)
+	{	    
+	    var exist=false,v="",aux=[].concat(ar),r=Array();
+	    for (var i in aux){
+	        v=aux[i];
+	        exist=false;
+	        for (var a in aux){	            
+	            if ($.trim(v) == $.trim(aux[a]) ){
+	                if (exist==false){
+	                    exist=true;
+	                }
+	                else{
+	                    aux[a]="";
+	                }
+	            }
+	        }
+	    }	    
+	    for (var a in aux){
+	        if ($.trim(aux[a])!=""){
+	            r.push(aux[a]);
+	        }
+	    }	    
+	    return r;
+	} 
 } (jQuery, window, document);
